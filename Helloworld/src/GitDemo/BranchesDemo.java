58ee4e2ea3eb28fc058d9a06cf7b0b86652c1ceb
rajvpatil5 <rajvpatil5@gmail.com>
2020-06-18 22:41:21 +0530

    d

--- Helloworld/src/GitDemo/BranchesDemo.java
+++ Helloworld/src/GitDemo/BranchesDemo.java
@@ -3,15 +3,9 @@ package GitDemo;
 public class BranchesDemo {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("hi I am the one from developrajat branch");
 		System.out.println("master branch");
-||||||| 485eb07
-		System.out.println("hi I am the one from developrajat branch");
-=======
-		System.out.println("hi I am the one from developrajat branchp");
-		System.out.println("developRajat");
->>>>>>> developrajat
+
 	}
 
 }
